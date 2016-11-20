354ff3c64846260d77b37ad0107086b728a6228b
Alexandra Govorkova <al.govorkova@gmail.com>
2016-11-20 15:56:13 +0300

    Show info window

--- js/views/map-view.js
+++ js/views/map-view.js
@@ -3,6 +3,36 @@ var app = app || {};
 (function () {
 	'use strict';
 
+    const PLACES = [
+        'amusement_park',
+        'aquarium',
+        'art_gallery',
+        'bar',
+        'beauty_salon',
+        'book_store',
+        'bowling_alley',
+        'cafe',
+        'casino',
+        'cemetery',
+        'church',
+        'city_hall',
+        'clothing_store',
+        'hindu_temple',
+        'jewelry_store',
+        'library',
+        'mosque',
+        'movie_theater',
+        'museum',
+        'night_club',
+        'park',
+        'place_of_worship',
+        'restaurant',
+        'shopping_mall',
+        'spa',
+        'synagogue',
+        'zoo'
+    ];
+
 	app.MapView = Backbone.View.extend({
 		el: '#map',
 
@@ -12,6 +42,7 @@ var app = app || {};
 			this.$input = $('#input');
             this.markers = [];
             this.currentMarker = null;
+            this.infoWin = null;
 
 			this.listenTo(app.places, 'add', this.addPlace);
 
@@ -19,6 +50,7 @@ var app = app || {};
                 center: {lat: 50, lng: 10},
                 zoom: 5
             });
+            this.placeService = new google.maps.places.PlacesService(this.map);
 
             // Set to the user position if there is no preset marker.
             !app.currentPlace && navigator.geolocation.getCurrentPosition(position => {
@@ -32,10 +64,24 @@ var app = app || {};
 		toMarker (id) {
             if (!id) { return; }
 
+            if (this.infoWin) {
+                // Close opened info window
+                this.infoWin.close();
+                this.infoWin = null;
+            }
+
             this.markers.some((marker) => {
                 // Find marker with id and focus on it
                 if (marker.id === id) {
                     this.map.setCenter(marker.getPosition());
+                    if (marker.placeInfo) {
+                        // Show infoWindow for this place
+                        this.infoWin = new google.maps.InfoWindow({
+                            content: marker.placeInfo
+                        });
+                        this.infoWin.open(this.map, marker);
+                    }
+
                     return true;
                 }
             });
@@ -60,10 +106,14 @@ var app = app || {};
                     });
 
                     // Set the input below the marker
-                    this.$input.css({
+                    let offset = e.pixel ? {
                         top: e.pixel.y,
-                        left: e.pixel.x - this.$input.width() / 2
-                    });
+                        left: e.pixel.x
+                    } : {
+                        top: e.sa.clientY,
+                        left: e.sa.clientX
+                    };
+                    this.$input.css(offset);
                     this.$input.addClass('visible');
                     this.$input.focus();
                 });
@@ -104,6 +154,38 @@ var app = app || {};
             marker.addListener('click', (param) => {
                 app.router.navigate(`#${id}`, {trigger: true});
             });
+
+            this.placeService.nearbySearch({
+                location: position,
+                types: PLACES,
+                rankBy: google.maps.places.RankBy.DISTANCE
+            }, (results, status) => {
+                let content, img = '';
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    // Set the closest result
+                    let place = results[0];
+                    content = `Place name: ${place.name}`;
+                    if (place.photos.length) {
+                        let url = place.photos[0].getUrl({
+                            maxWidth: 150,
+                            maxHeight: 150
+                        });
+                        img = `<div class="img"><img src="${url}" /></div>`;
+                    }
+                } else {
+                    content = 'No information about this place';
+                }
+
+                marker.placeInfo = `
+                    <div class="info">
+                        ${img}
+                        <div class="content">
+                            <h1>${marker.title}</h1>
+                            <p>${content}</p>
+                        </div>
+                    </div>`;
+            });
+
             this.markers.push(marker);
         }
 	});
